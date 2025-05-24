8e779f2e5989dd10153a7fc0e7e9db3b5d074e07
Alejandro.Gavilanes <alejandrogavilanes@gmail.com>
2025-05-24 19:56:55 +0100

    first commit

---
+++ db-scripts/create-tables.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test (id INT, name VARCHAR(255));
+INSERT INTO test VALUES (1, 'postgres');
+INSERT INTO test VALUES (2, 'pg-admin');
+INSERT INTO test VALUES (3, 'docker');
+SELECT * FROM test;
